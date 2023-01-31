529d12d4a862f1144fc9412ce192360a2523a8d6
NataliaG0r <123062281+NataliaG0r@users.noreply.github.com>
2023-01-31 15:33:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿int numberA = 5;
+int numberB = 7;
+if (numberA > numberB)
+{
+    Console.WriteLine($"число {numberA} больше,чем число {numberB}");
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine($"число {numberB} больше,чем число {numberA}");
+}
+else
+{
+    Console.WriteLine($"числа равны");
+}
+
+int numberC = 2;
+int numberD = 10;
+if (numberC > numberD)
+{
+    Console.WriteLine($"число {numberC} больше,чем число {numberD}");
+}
+else if (numberC < numberD)
+{
+    Console.WriteLine($"число {numberD} больше,чем число {numberC}");
+}
+else
+{
+    Console.WriteLine($"числа равны");
+}
+
+int number1 = -9;
+int number2 = -3;
+if (number1 > number2)
+{
+    Console.WriteLine($"число {number1} больше,чем число {number2}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"число {number2} больше,чем число {number1}");
+}
+else
+{
+    Console.WriteLine($"числа равны");
+}
